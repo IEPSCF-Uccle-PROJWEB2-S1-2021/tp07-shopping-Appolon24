b5fca4cc8d12db1d5af5a5d8e3ba5aab8febade0
Appolon24 <71607931+Appolon24@users.noreply.github.com>
2020-10-26 20:07:57 +0100

    done

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Écrivez votre code JavaScript ici.
+    
+let clk = document.querySelector('button');
+let btn = document.querySelector('#input');
+function operation()
+{
+let liste = document.getElementById('items').value;
+let ok = liste;
+document.getElementById('items').value = "";
+
+var list = document.createElement('li');
+btn.appendChild(list);
+var sp = document.createElement('span');
+var but = document.createElement('button');
+
+list.appendChild(but);
+list.appendChild(sp);
+let tex = document.createTextNode(ok);
+sp.appendChild(tex);
+
+var intern = document.createTextNode("Delete");
+but.appendChild(intern);
+}
+clk.addEventListener('click', operation);
